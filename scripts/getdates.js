92f3c72749c8eed56bd904c7e47da0ac3748fdf2
jonah-alldredge <jalldredge1@byupathway.edu>
2025-11-04 15:54:50 -0700

    updated page

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+// Create a new Date object
+const lastModified = new Date();
+
+// // Format it as a readable string
+// const dateTime = lastModified.toLocaleString();
+
+// // Display it on the page
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
