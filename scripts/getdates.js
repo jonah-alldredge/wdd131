1b7f7d2791ade1d83991e0fee378f8717ea72a35
jonalldr <jonahalldredge1@gmail.com>
2026-03-11 10:07:32 -0600

    made the copyright year display with Javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,10 @@
 // Create a new Date object
 const lastModified = new Date();
+const currentYear = new Date().getFullYear();
 
 // // Format it as a readable string
 // const dateTime = lastModified.toLocaleString();
 
 // // Display it on the page
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+document.getElementById("currentYear").textContent =  currentYear;
