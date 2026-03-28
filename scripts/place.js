e25f1912f82008c610fe5d8a3912959bf804c45b
jonalldr <jonahalldredge1@gmail.com>
2026-03-27 23:52:49 -0600

    finished javascript

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,22 @@ const currentYear = new Date().getFullYear();
 // // Display it on the page
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
 document.getElementById("currentYear").textContent = currentYear;
+
+const temperature = 12;
+const windSpeed = 5;
+
+const setWindChill = document.getElementById("windChillText");
+const setTemperature = document.getElementById("temperatureText").textContent = `Temperature: ${temperature}\u00B0C`;
+const setWindSpeed = document.getElementById("windText").textContent = `Wind: ${windSpeed} mph`;
+
+function calculateWindChill(t, w) {
+    let calcWindChill = Math.round(35.74 + (0.6215 * t) - (35.75 * (Math.pow(w, 0.16))) + (0.4275 * t * (Math.pow(w, 0.16))));
+    return calcWindChill;
+}
+
+if (temperature <= 50 && windSpeed > 3){
+    const windChill = calculateWindChill(temperature, windSpeed);
+    setWindChill.textContent = `Wind Chill: ${windChill}\u00B0C`;
+} else {
+    setWindChill.textContent = "Wind Chill: NA";
+}
