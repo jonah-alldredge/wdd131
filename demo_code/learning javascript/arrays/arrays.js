bea0b6679fa8faca9c55c31dbafd23c3849cabbb
jonalldr <jonahalldredge1@gmail.com>
2026-04-08 16:18:49 -0600

    halfway done with form assignment

--- demo_code/learning javascript/arrays/arrays.js
+++ demo_code/learning javascript/arrays/arrays.js
@@ -61,3 +61,132 @@ const namesReduce = length.reduce((accumulator, currentValue) => accumulator + c
 // EXAMPLE ANSWER
 console.log(names.reduce((total, name) => total + name.length, 0) / names.length);
 console.log(namesReduce);
+
+
+// CALLBACK WITH ARRAY METHODS
+
+// forEach()
+const nums = [1, 2, 3, 4, 5];
+
+nums.forEach(function (num) {
+    num = num * num;
+    console.log(num); 
+});
+// Iterate an Object
+const employees = [
+    {
+        id: 101,
+        name: 'Alex',
+        sal: 10000
+    },
+    {
+        id: 102,
+        name: 'Clark',
+        sal: 13000
+    },
+    {
+        id: 103,
+        name: 'John',
+        sal: 12000
+    },
+    {
+        id: 104,
+        name: 'Tony',
+        sal: 20000
+    }
+]
+
+employees.forEach(function (emp) {
+    console.log(emp.name.toUpperCase()); 
+});
+
+// map()
+const squareNums = nums.map(function (num) {
+    return num * num; 
+});
+console.log(squareNums);
+
+
+const employee = employees.map(function (emp) {
+    return emp.name;
+});
+console.log(employee);
+
+
+const shortWords = ['lol', 'brb', 'btw', 'lmk', 'g2g'];
+const abbreviations = shortWords.map(function (word) {
+    return word.toUpperCase().split('').join('.'); 
+});
+console.log(abbreviations);
+
+
+// ARROW FUNCTIONS
+const square = (num) => {
+    return num * num;
+}
+// Without parenthesis - they are optional for one argument
+const squareNum = num => {
+    return num * num;
+}
+// Implicit Return - one liner
+const squareNumber = num => num * num;
+
+
+// find()
+const fullNames = ['Jimmy Shah', 'Ram Sharma', 'Vikram Gupta', 'Shyam Sharma'];
+const nameResult = fullNames.find(name => name.includes('Sharma'))
+console.log(nameResult);
+
+const salary = employees.find(emp => emp.sal >= 20000);
+console.log(salary);
+
+
+// filter()
+const filterNames = fullNames.filter(name => name.includes('Sharma'));
+console.log(filterNames);
+
+const filterSal = employees.filter(emp => emp.sal >= 20000);
+console.log(filterSal);
+
+
+// every()
+const kWords = ['kia', 'karen', 'king', 'korean'];
+// Check all words starts with k
+console.log(kWords.every(word => word[0] === 'k'));
+// Check all words ends with g
+console.log(kWords.every(word => word[kWords.length - 1] === 'g'));
+
+
+// some() - very similar but returns true if any of the items pass the given function
+console.log(kWords.some(word => word[kWords.length - 1] === 'g')); // was false with every() but returns true with some()
+
+
+
+// sort()
+const numberList = [1, 20, 10, 22, 30, 44, 100, 11];
+numberList.sort(); // [1, 10, 100, 11, 20 22, 30, 44]
+numberList.sort((a, b) => a - b); // [1, 10, 11, 20, 22, 30, 44, 100]
+
+
+// reduce()
+// Sum of the array
+let output = numberList.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(output);
+// Finding the min/max
+output = numberList.reduce((max, item) => Math.max(max, item));
+console.log(output); //100
+// Tallying Votes
+const votes = ['y', 'y', 'n', 'y', 'n', 'n', 'n'];
+output = votes.reduce((tally, vote) => {
+    tally[vote] = (tally[vote] || 0) + 1
+    return tally;
+}, {}); // Initial Value: {}
+
+function calculate(a, b, callback) {
+    callback(a + b);
+}
+
+function displayResult(result) {
+    console.log("The result is: " + result);
+}
+calculate(2, 3, displayResult); // the answer for callback essentially becomes the result
